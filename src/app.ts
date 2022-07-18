f68bed0a49e8e1a38cc0a9f0a4f3d7b15aa361e3
francis-mwas <mwangif184@gmail.com>
2022-07-18 23:08:01 +0300

    Added app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,57 @@
-import express, {Application} from "express";
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import Controller from '@/utils/interfaces/controller.interface';
+import ErrorMiddleware from '@/middleware/error.middleware';
+
+class App {
+    public express: Application;
+    public port: number;
+
+    constructor(controllers: Controller[], port: number) {
+        this.express = express();
+        this.port = port;
+
+        this.intializeDatabaseConnection();
+        this.initialiseMiddleware();
+        this.initialiseControllers(controllers);
+        this.initialiseErrorHandling();
+    }
+
+    private initialiseMiddleware(): void {
+        this.express.use(helmet());
+        this.express.use(cors());
+        this.express.use(morgan('dev'));
+        this.express.use(express.json());
+        this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(compression());
+    }
+    private initialiseControllers(controllers: Controller[]): void {
+        controllers.forEach((controller: Controller) => {
+            this.express.use('/api', controller.route);
+        });
+    }
+
+    private initialiseErrorHandling(): void {
+        this.express.use(ErrorMiddleware());
+    }
+
+    private intializeDatabaseConnection(): void {
+        const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+
+        mongoose.connect(
+            `mongodb://${MONGO_USER}:${MONGO_PASSWORD}:${MONGO_PATH}`
+        );
+    }
+
+    public listen(): void {
+        this.express.listen(this.port, () => {
+            console.log(`The app is lisetning on port: ${this.port}`);
+        });
+    }
+}
+
+export default App;
